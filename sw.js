5ff1ee438384cc7dcae07ef0fa9a5f76d2b22f3e
Omar Bouallou <omar@bouallou.com>
2020-04-20 00:04:18 +0200

    initial commit

---
+++ sw.js
@@ -0,0 +1,69 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "certificate.1dd6d3ca.js",
+    "revision": "5d7118e7f8e9a65152bf00d6742a96cf"
+  },
+  {
+    "url": "certificate.84dda806.pdf",
+    "revision": "343ac8bb3542a6578f2a4cbaf0f99679"
+  },
+  {
+    "url": "certificate.b7dcfaaf.css",
+    "revision": "de5506d7c1617c3893421a3c2d405958"
+  },
+  {
+    "url": "generate.f0102810.js",
+    "revision": "d2603c77f93648a3edabc00f4a656152"
+  },
+  {
+    "url": "index.html",
+    "revision": "4230d828131064b09e71301de0e0cc29"
+  },
+  {
+    "url": "marianne-bold-webfont.1505950c.woff2",
+    "revision": "e67f6cefe32cc39f909e605c8d6337a9"
+  },
+  {
+    "url": "marianne-bold-webfont.7424dbde.woff",
+    "revision": "0bcc99dd4adfb78e11098fedfe531cbb"
+  },
+  {
+    "url": "marianne-regular-webfont.0a959359.woff",
+    "revision": "89f4f2326c77429e98693cf83703face"
+  },
+  {
+    "url": "marianne-regular-webfont.daa94941.woff2",
+    "revision": "d2c09e5f58d8360f541e2a8726c33587"
+  },
+  {
+    "url": "./",
+    "revision": "0b36113f5f53ee19dca0682ad4eaca44"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/deplacement-covid-19/index.html"));
